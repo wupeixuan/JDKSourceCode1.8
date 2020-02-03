cf9d7bd589c346d91fe6d23246ea0989f2581274
wupeixuan <18603203910@163.com>
2020-02-03 17:50:51 +0800

    任何 class 只要实现了 Runnable，就能被线程执行

--- src/java/lang/Runnable.java
+++ src/java/lang/Runnable.java
@@ -1,69 +1,31 @@
-/*
- * Copyright (c) 1994, 2013, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
 package java.lang;
 
 /**
- * The <code>Runnable</code> interface should be implemented by any
- * class whose instances are intended to be executed by a thread. The
- * class must define a method of no arguments called <code>run</code>.
- * <p>
- * This interface is designed to provide a common protocol for objects that
- * wish to execute code while they are active. For example,
- * <code>Runnable</code> is implemented by class <code>Thread</code>.
- * Being active simply means that a thread has been started and has not
- * yet been stopped.
- * <p>
- * In addition, <code>Runnable</code> provides the means for a class to be
- * active while not subclassing <code>Thread</code>. A class that implements
- * <code>Runnable</code> can run without subclassing <code>Thread</code>
- * by instantiating a <code>Thread</code> instance and passing itself in
- * as the target.  In most cases, the <code>Runnable</code> interface should
- * be used if you are only planning to override the <code>run()</code>
- * method and no other <code>Thread</code> methods.
- * This is important because classes should not be subclassed
- * unless the programmer intends on modifying or enhancing the fundamental
- * behavior of the class.
+ * 任何 class 只要实现了 Runnable，就能被线程执行。
+ * 定义了一个通用的标准，只要想被运行，就实现它。
+ * 可以在不开子线程的情况下运行
+ * Runnable 表示一类无返回值，且不抛出异常的任务
+ * Runnable 类任务通常由 Thread 直接执行，也可以交给【任务执行器】Executor去执行
+ * 此外，Runnable 还可以经过适配器的装配，与 Callable 类型、Future 类型等配合使用
+ * 该接口已函数化：
+ * Runnable runnable = new Runnable() {
+ *     @Override
+ *     public void run() {
+ *         System.out.println("Runnable");
+ *     }
+ * };
+ * 可以简写为：
+ * Runnable runnable = () -> { System.out.println("Runnable"); };
  *
- * @author  Arthur van Hoff
- * @see     java.lang.Thread
- * @see     java.util.concurrent.Callable
- * @since   JDK1.0
+ * @see java.lang.Thread
+ * @see java.util.concurrent.Callable
  */
 @FunctionalInterface
 public interface Runnable {
     /**
-     * When an object implementing interface <code>Runnable</code> is used
-     * to create a thread, starting the thread causes the object's
-     * <code>run</code> method to be called in that separately executing
-     * thread.
-     * <p>
-     * The general contract of the method <code>run</code> is that it may
-     * take any action whatsoever.
+     * 任务执行入口
      *
-     * @see     java.lang.Thread#run()
+     * @see java.lang.Thread#run()
      */
-    public abstract void run();
+    void run();
 }
